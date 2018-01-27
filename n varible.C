eaf05d2749258af8509353fcc66e8966a80642ff
balaji181 <35652175+balaji181@users.noreply.github.com>
2018-01-27 04:43:16 +0000

    Create n varible.C

---
+++ n varible.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i;
+    printf("Enter The Value for a\n");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    printf("\n hello");
+    }
