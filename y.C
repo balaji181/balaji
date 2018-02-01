2ba60097d3b68a3aa2a96435fe76d2cb3af7b4d3
balaji181 <35652175+balaji181@users.noreply.github.com>
2018-02-01 16:41:22 +0530

    Update and rename n.C to y.C

--- y.C
+++ y.C
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-    int i,n,a,sum=0;
+    int i,n,b,sum=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a);
-        sum=sum+a;
+        scanf("%d",&b);
+        sum=sum+b;
     }
     printf("%d",sum);
 }
